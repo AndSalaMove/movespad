73fe7849730edc66e9bcae341c3a29a949bad443
Andrea Sala <andrea.sala@movesolutions.it>
2023-04-03 10:55:51 +0200

    Added photon energy param

---
+++ src/movespad/params.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# TIME: s
+# SPACE: m
+
+PI = np.pi
+FF = .265
+TAU_OPT = .66
+RHO_TGT = .75
+F_LENS = .006
+D_LENS = .005
+F_HASH = F_LENS / D_LENS
+THETA_E_DEG = 1.7
+THETA_E_RAD = THETA_E_DEG * PI / 180
+
+SIGMA_LASER = 250e-12 / 2.355
+PULSE_ENERGY = 6.2e-12
+PIXEL_AREA = 3600e-12
+Z = 1 #m
+
+PHOTON_WAVEL = 405e-9
+H = 6.62607015e-34 # in SI units 
+C = 299792458
+E_PH = H * C / PHOTON_WAVEL # photon energy
