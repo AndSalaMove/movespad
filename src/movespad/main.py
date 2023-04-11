0b150694ccaec97a59510076659b4f6008d04b4b
Andrea Sala <andrea.sala@movesolutions.it>
2023-04-11 15:25:53 +0200

    update main

--- src/movespad/main.py
+++ src/movespad/main.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from time import time
+import timeit
 from movespad import laser, bkg, spad, params as pm, pixel
 from movespad import spad
 
 
 def main():
 
-    start = time()
+
     offset = 2* pm.Z / pm.C
     time_step = 50e-12
     start, stop = 0, 4e-6
@@ -15,23 +16,22 @@ def main():
     times = np.linspace(start, stop, n_steps)
     bw = 1 #points --> 50 ps
 
-    print(offset)
-
     bkg_spec = bkg.bkg_spectrum(times)
     las_spec = laser.full_laser_spectrum(times, stop, init_offset=offset)
 
     n_ph_las, t_laser = laser.get_n_photons(times, las_spec, bw)
     n_ph_bkg, t_bkg = bkg.get_n_photons_bkg(times, bkg_spec, bw)
 
-    print(f"Total laser photons: {sum(n_ph_las)}")
-    print(f"Total bkg photons: {sum(n_ph_bkg)}")
+    # print(f"Total laser photons: {sum(n_ph_las)}")
+    # print(f"Total bkg photons: {sum(n_ph_bkg)}")
 
     pix = pixel.Pixel(size = 2)
     pix.create_and_split(t_laser, t_bkg, pm.PDP)
+    pix.crosstalk(pm.XTALK_PROBS)
     pix.process_events(pm.T_DEAD, pm.PDP, pm.AP_PROB)
 
-    print(f"Total after deadtime filter: {[len(ts) for ts in pix.timestamps]}")
-    breakpoint()  
+    print(f"Photon count: {[len(ts.timestamps) for ts in pix.timestamps]}")
+    print(f"Laser count: {[len([elem for elem in ts.timestamps if elem.type=='las']) for ts in pix.timestamps]}")
     pix.plot_events(times, las_spec)
 
 
