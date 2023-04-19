5fc968b5a8a192868775cb2d3c4479818e73996f
Andrea Sala <andrea.sala@movesolutions.it>
2023-04-19 16:26:48 +0200

    Develop basic gui

--- src/movespad/gui.py
+++ src/movespad/gui.py
@@ -1,45 +1,53 @@
 import PySimpleGUI as sg
 import os, pathlib
+import movespad
 
 
 def gui():
 
     sg.set_options(font=("Helvetica", 12))
-    base_path = pathlib.Path(__file__).parent.parent.parent.absolute()
-    img_path = os.path.join(base_path, "img", "movex.png")
+
     sg.theme("DarkBlue15")
     layout = [
         #[sg.Image(img_path, size=(1000, 500))],
         [sg.T("MOVE-X LIDAR SIMULATION", justification='center', size=(60,2), font=("Helvetica", 14, "bold"))],
 
-        [sg.T("Laser specifications", justification='center', size=(30,1), font=("Helvetica", 12, "bold"))],
+        [sg.T("Laser specifications", justification='center', size=(30,1), font=("Helvetica", 12, "bold")),
+         sg.T("Pixel specifications", justification='center', size=(30,1), font=("Helvetica", 12, "bold"))],
 
-        [sg.T("Peak Power (W)", size=(20,1), justification='right'), sg.I(key='laser_power', default_text='60', size=(10,1))],
-        [sg.T("Sigma (ns)", size=(20,1), justification='right'),     sg.I(key='laser_sigma', default_text='1.9108', size=(10, 1))],
-        [sg.T("Pulse distance (us)", size=(20,1), justification='right'),         sg.I(key='pulse_distance', default_text='1.35', size=(10,1))],
+        [sg.T("Peak Power (W)", size=(20,1), justification='right'), sg.I(key='laser_power', default_text='60', size=(10,1)),
+         sg.T("SPADs per side", size=(20, 1), justification='right'), sg.I(key='pixel_size', default_text='3', size=(10,1))],
+        [sg.T("Sigma (ns)", size=(20,1), justification='right'),     sg.I(key='laser_sigma', default_text='1.9108', size=(10, 1)),
+         sg.T("Fill factor", size=(20,1), justification='right'), sg.I(key='fill_factor', default_text='0.99', size=(10,1))],
+        [sg.T("Pulse distance (us)", size=(20,1), justification='right'), sg.I(key='pulse_distance', default_text='1.35', size=(10,1)),
+         sg.T("SPAD size (um)", size=(20,1), justification='right'), sg.I(key='spad_size', default_text="10.17", size=(10,1))],
+        [sg.T("Beam divergence (mrad)", size=(20,1), justification="right"), sg.I(key='theta_h', size=(4,1), default_text="1.5"),
+         sg.T("x") ,sg.I(key='theta_v', size=(4,1), default_text="1.5"), sg.T("PDP", justification='right', size=(18,1)),
+         sg.I(key='PDP', default_text="0.2", size=(10,1))],
         [sg.T("")],
-        
-        [sg.T("Optics", justification='center', size=(30,1), font=("Helvetica", 12, "bold"))],
-        
-        [sg.T("Focal length (mm)", size=(20,1), justification='right'), sg.I(key='f_lens', default_text='5')],
-        [sg.T("Lens diameter (mm)", size=(20,1), justification='right'), sg.I(key='d_lens', default_text='6')],
-        [sg.T("Transmittance", size=(20,1), justification='right'), sg.I(key='tau', default_text='0.90')],
+        [sg.T("Optics", justification='center', size=(30,1), font=("Helvetica", 12, "bold")),
+         sg.T("Other" , justification='center', size=(30,1), font=("Helvetica", 12, "bold"))],
 
-        [sg.T("Pixel specifications", justification='center', size=(35,2)), sg.T("Target specifications", justification='center', size=(35, 2))],
-        [sg.T("Pixel size (SPADs per size)", size=(10,1)), sg.I(key='pixel_size', default_text='3', size=(7,1)), sg.T("Reflectivity", size=(10,1)), sg.I(key='rho_tgt', default_text="0.20", size=(7,1))],
-        [sg.T("Fill factor"), sg.I(key='fill_factor', default_text='0.99')],
-        [sg.Submit(), sg.Cancel()]
+        [sg.T("Focal length (mm)", size=(20,1), justification='right'),  sg.I(key='f_lens', default_text='5', size=(10,1)),
+        sg.T("Bkg irradiance (W/m2)", size=(20,1), justification='right'), sg.I(key="bkg_power", default_text="0.2", size=(10,1))],
+        [sg.T("Lens diameter (mm)", size=(20,1), justification='right'), sg.I(key='d_lens', default_text='6', size=(10,1)),
+        sg.T("T dead (ns)", size=(20,1), justification='right'), sg.I(key="t_dead", default_text="7", size=(10,1))],
+        [sg.T("Transmittance", size=(20,1), justification='right'),      sg.I(key='tau', default_text='0.90', size=(10,1)),
+        sg.T("Coincidence number", size=(20,1), justification='right'), sg.I(key="coinc_thr", default_text="6", size=(10,1))],
+
+        [sg.T("", size=(30,5))],
+        [sg.Text("", size=(30)), sg.Submit(auto_size_button=True), sg.Cancel(auto_size_button=True)]
 
     ]
 
-    window = sg.Window("LIDAR starter", layout=layout)
+    window = sg.Window("LIDAR starter", layout=layout, resizable=True, )
 
     event, values = window.read()
 
     if event=='Cancel':
         window.close()
     elif event=='Submit':
-        print(values) 
+        movespad.main.execute_main()
 
 if __name__ == '__main__':
     gui()
